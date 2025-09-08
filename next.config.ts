bc02935358f2c50bf7988336492591e0b77a1ad8
Talalmahmud <mahmudtalal2@gmail.com>
2025-09-08 10:25:09 +0600

    feat: update image domains in next.config.ts for consistency

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "images.unsplash.com",
-      "cut-x.ekopiidev.com",
-      "ecommerce.ekopiidev.com",
-    ],
+    domains: ["images.unsplash.com", "cut-x.ekopiidev.com", "ekopiidev.com"],
   },
 };
 
